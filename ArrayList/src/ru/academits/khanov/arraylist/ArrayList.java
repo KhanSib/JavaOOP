124f8e45a5eb876ea2192410bb26ee296ae466e7
89134550004@mail.ru <89134550004@mail.ru>
2020-07-24 14:54:39 +0700

    ArrayList

---
+++ ArrayList/src/ru/academits/khanov/arraylist/ArrayList.java
@@ -0,0 +1,153 @@
+package ru.academits.khanov.arraylist;
+
+import java.util.*;
+
+public class ArrayList<T> implements List<T> {
+    private T[] items;
+    private int length;
+
+    public ArrayList(T[] items) {
+        this.items = Arrays.copyOf(items, items.length);
+        length = items.length;
+    }
+ 
+    @Override
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        for (T item : items) {
+            if (item.equals(o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private class ArrayListIterator implements Iterator<T> {
+        private int currentIndex = -1;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < length;
+        }
+
+        @Override
+        public T next() {
+            currentIndex++;
+            return items[currentIndex];
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayListIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return items;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
