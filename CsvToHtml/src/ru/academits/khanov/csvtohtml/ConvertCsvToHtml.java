57f08f7ce2937f297a13813f598a2c5264e120ee
89134550004@mail.ru <89134550004@mail.ru>
2020-07-29 14:54:56 +0700

    Добавил CsvToHtml

---
+++ CsvToHtml/src/ru/academits/khanov/csvtohtml/ConvertCsvToHtml.java
@@ -0,0 +1,162 @@
+package ru.academits.khanov.csvtohtml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ConvertCsvToHtml {
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileInputStream(args[0]));
+             PrintWriter writer = new PrintWriter("output.html")) {
+
+            boolean isTableRowOpen = false;
+            boolean isTableDetailOpen = false;
+            boolean isTableDetailShielded = false;
+            int doubleQuotesCount = 0;
+
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<title>!DOCTYPE</title>");
+            writer.println("<meta charset=\"utf-8\">");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<table border=\"1\">");
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (!isTableRowOpen) {
+                    writer.println("<tr>");
+                    isTableRowOpen = true;
+                }
+
+                if (!isTableDetailOpen) {
+                    writer.print("<td>");
+                    isTableDetailOpen = true;
+                }
+
+                int i = 0;
+
+                while (i < line.length()) {
+                    if (!isTableDetailOpen) {
+                        writer.print("<td>");
+                        isTableDetailOpen = true;
+                    }
+
+                    if (line.charAt(i) == '<') {
+                        writer.print("&lt;");
+                        i++;
+                    }
+
+                    if (line.charAt(i) == '>') {
+                        writer.print("&gt;");
+                        i++;
+                    }
+
+                    if (line.charAt(i) == '&') {
+                        writer.print("&amp;");
+                        i++;
+                    }
+
+                    if (line.charAt(i) == ',') {
+                        if (isTableDetailShielded && doubleQuotesCount % 2 == 0) {
+                            writer.print("</td>");
+                            isTableDetailOpen = false;
+                            isTableDetailShielded = false;
+                            doubleQuotesCount = 0;
+                            i++;
+                            continue;
+                        }
+
+                        if (!isTableDetailShielded) {
+                            writer.print("</td>");
+                            isTableDetailOpen = false;
+                            i++;
+                            continue;
+                        }
+
+                        writer.print(",");
+                        i++;
+                        continue;
+                    }
+
+                    if (line.charAt(i) == '"') {
+                        if (!isTableDetailShielded) {
+                            doubleQuotesCount++;
+                            isTableDetailShielded = true;
+                            i++;
+                            continue;
+                        }
+
+                        doubleQuotesCount++;
+
+                        if (i + 1 < line.length() - 1) {
+                            if (line.charAt(i + 1) == '"') {
+                                writer.print("\"");
+                                doubleQuotesCount++;
+                                i += 2;
+                                continue;
+                            }
+
+                            if (line.charAt(i + 1) == ',' && doubleQuotesCount % 2 != 0) {
+                                writer.print(",");
+                                i += 2;
+                                continue;
+                            }
+
+                            writer.print("</td>");
+                            isTableDetailOpen = false;
+                            isTableDetailShielded = false;
+                            doubleQuotesCount = 0;
+                            i += 2;
+                            continue;
+                        }
+
+                        i++;
+                        continue;
+                    }
+
+                    writer.print(line.charAt(i));
+                    i++;
+                }
+
+                if (isTableDetailShielded && doubleQuotesCount % 2 == 0) {
+                    writer.print("</td>");
+                    isTableDetailOpen = false;
+
+                    if (isTableRowOpen = true) {
+                        writer.println();
+                        writer.print("</tr>");
+                        isTableRowOpen = false;
+                    }
+
+                    isTableDetailShielded = false;
+                    doubleQuotesCount = 0;
+
+                    continue;
+                }
+
+                if (isTableDetailOpen) {
+                    if (!isTableDetailShielded) {
+                        writer.print("</td>");
+                        isTableDetailOpen = false;
+                    } else {
+                        writer.print("<br/>");
+                    }
+                }
+
+                if (!isTableDetailShielded) {
+                    writer.println();
+                    writer.println("</tr>");
+                    isTableRowOpen = false;
+                }
+            }
+
+            writer.println("</table>");
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
+}
