c98572818d103fe880cceadcc41f3802d296075d
89134550004@mail.ru <89134550004@mail.ru>
2020-08-19 12:23:18 +0700

    ArrayListHome 16.08.2020

--- ArrayListHome/src/ru/academits/khanov/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/khanov/arraylisthome/Main.java
@@ -5,38 +5,39 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ArrayList<String> fileList = new ArrayList<>();
+        ArrayList<String> fileLinesList = new ArrayList<>();
 
         try (FileInputStream fileInputStream = new FileInputStream("input.txt")) {
             Scanner scanner = new Scanner(fileInputStream);
 
             while (scanner.hasNextLine()) {
-                fileList.add(scanner.nextLine());
+                fileLinesList.add(scanner.nextLine());
             }
         } catch (FileNotFoundException e) {
             System.out.println("Error: отсутствует файл для считывания строк");
         }
 
-        System.out.println("Список строк из файла: " + fileList);
+        System.out.println("Список строк из файла: " + fileLinesList);
 
         ArrayList<Integer> integersList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-        for (int i = 0; i < integersList.size(); i++) {
-            Integer integer = integersList.get(i);
+        Iterator<Integer> iterator = integersList.iterator();
 
-            if (integer % 2 == 0) {
-                integersList.remove(integer);
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 == 0) {
+                iterator.remove();
             }
         }
 
         System.out.println("Список без четных чисел: " + integersList);
 
         ArrayList<Integer> listWithDuplicates = new ArrayList<>(Arrays.asList(4, 2, 1, 2, 3, 4, 5, 4, 2, 1));
-        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>();
+        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>(10);
 
         for (Integer integer : listWithDuplicates) {
             if (!listWithoutDuplicates.contains(integer)) {
