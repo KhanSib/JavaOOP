c1b5638e03df8cb41e40635d940fe940d51ea40d
89134550004@mail.ru <89134550004@mail.ru>
2020-08-31 11:42:00 +0700

    ArrayListHome 25.08.2020

--- ArrayListHome/src/ru/academits/khanov/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/khanov/arraylisthome/Main.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
@@ -26,18 +25,17 @@ public class Main {
 
         ArrayList<Integer> integersList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-        Iterator<Integer> iterator = integersList.iterator();
-
-        while (iterator.hasNext()) {
-            if (iterator.next() % 2 == 0) {
-                iterator.remove();
+        for (int i = 0; i < integersList.size(); i++) {
+            if (integersList.get(i) % 2 == 0) {
+                integersList.remove(i);
+                i--;
             }
         }
 
         System.out.println("Список без четных чисел: " + integersList);
 
         ArrayList<Integer> listWithDuplicates = new ArrayList<>(Arrays.asList(4, 2, 1, 2, 3, 4, 5, 4, 2, 1));
-        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>(10);
+        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>(listWithDuplicates.size());
 
         for (Integer integer : listWithDuplicates) {
             if (!listWithoutDuplicates.contains(integer)) {
